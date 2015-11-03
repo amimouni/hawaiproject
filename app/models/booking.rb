750b454564a8af82d28232f8ae37cbbae00365cf
sergio monteiro <sergio.perso@gmail.com>
2015-11-03 19:05:14 +0100

    added booking creation on livingrooms

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,5 +1,5 @@
 class Booking < ActiveRecord::Base
   belongs_to :livingroom
   belongs_to :user
-  validates :total_amount, :arrival_date, :departure_date, presence: true
+  validates :total_amount, :arrival_date, :departure_date, :user_id, :livingroom_id, presence: true
 end
