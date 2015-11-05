baf50c274b0a6158113e6411c3d80be8a5bcba29
sergio monteiro <sergio.perso@gmail.com>
2015-11-05 18:10:52 +0100

    finishing-booking-process

--- app/models/livingroom.rb
+++ app/models/livingroom.rb
@@ -1,6 +1,6 @@
 class Livingroom < ActiveRecord::Base
   belongs_to :user
-  has_many :bookings
+  has_many :bookings, :dependent => :destroy
   has_many :pictures, :dependent => :destroy
   validates :capacity, :price, :city, :address, presence: true
 end
