2ec230594e889466209720d7107b43e95e53a0a7
sergio monteiro <sergio.perso@gmail.com>
2015-11-05 16:41:33 +0100

    add-images-upload

--- app/models/livingroom.rb
+++ app/models/livingroom.rb
@@ -1,6 +1,8 @@
 class Livingroom < ActiveRecord::Base
   belongs_to :user
   has_many :bookings
-
+  has_many :pictures, :dependent => :destroy
   validates :capacity, :price, :city, :address, presence: true
 end
+
+
